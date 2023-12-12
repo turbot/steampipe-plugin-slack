bd0ba0f59e0bb27f22e477b6772288f5cf4218a8
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-12 17:07:43 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#72)

--- slack/connection_config.go
+++ slack/connection_config.go
@@ -2,17 +2,10 @@ package slack
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type slackConfig struct {
-	Token *string `cty:"token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"token": {
-		Type: schema.TypeString,
-	},
+	Token *string `hcl:"token"`
 }
 
 func ConfigInstance() interface{} {
